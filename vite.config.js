af4a5ab2e77e6778aecbd784dcb22cefd97ba70d
Kristian Vines <kvines4@dxc.com>
2022-10-17 23:13:17 +1100

    move images to public assets folder to fix link issue add other pages fix nav links add routing config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,24 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
+const root = resolve(__dirname, 'src')
+
 export default defineConfig({
-    base: '/RMIT-IITA2/'
+	root,
+	base: '/RMIT-IITA2/',
+	publicDir: resolve(__dirname, 'assets'),
+	build: {
+		outDir: resolve(__dirname, 'dist'),
+		rollupOptions: {
+			input: {
+				main:			resolve(root, 'index.html'),
+				tools:			resolve(root, 'tools/index.html'),
+				industrydata:	resolve(root, 'industrydata/index.html'),
+				itwork:			resolve(root, 'itwork/index.html'),
+				ittechnologies:	resolve(root, 'ittechnologies/index.html'),
+				projectidea:	resolve(root, 'projectidea/index.html'),
+			}
+		},
+	},
 })
+
